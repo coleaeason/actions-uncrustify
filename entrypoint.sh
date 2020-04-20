ad6c2fedf0217cb7bc97d4d60178e46b716d520b
Cole Eason <cole@expensify.com>
2020-04-20 10:14:17 -0700

    Clean up output from script

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,30 +10,37 @@ BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 # from the actions file. Otherwise the actions file could have the
 # configPath argument set
 if [[ -z $UNCRUSTIFY_CONFIG ]] && [[ -z $INPUT_CONFIGPATH ]]; then
-    CONFIG="-c /default.cfg"
+    CONFIG=" -c /default.cfg"
 elif [[ -z $UNCRUSTIFY_CONFIG ]] && [[ -n $INPUT_CONFIGPATH ]]; then
-    CONFIG="-c $INPUT_CONFIGPATH"
+    CONFIG=" -c $INPUT_CONFIGPATH"
 # If both are set, use the command line flag.
 elif [[ -n $UNCRUSTIFY_CONFIG ]] && [[ -n $INPUT_CONFIGPATH ]]; then
-    CONFIG="-c $INPUT_CONFIGPATH"
+    CONFIG=" -c $INPUT_CONFIGPATH"
 elif [[ -n $UNCRUSTIFY_CONFIG ]] && [[ -z $INPUT_CONFIGPATH ]]; then
     CONFIG=""
 else
-    CONFIG="-c /default.cfg"
+    CONFIG=" -c /default.cfg"
 fi
 
 EXIT_VAL=0
 
 while read -r FILENAME; do
-    RETURN_VAL=$(uncrustify --check ${CONFIG} -f ${FILENAME} -l CPP)
-    if [[ "$RETURN_VAL" -gt "$EXIT_VAL" ]]; then
+    FAILED="false"
+    OUT=$(uncrustify --check${CONFIG} -f ${FILENAME} -l CPP | grep ^FAIL | awk '{ printf "%s failed style checks.\n", $2}')
+    RETURN_VAL=${PIPESTATUS[0]}
+    if [[ $RETURN_VAL -gt $EXIT_VAL ]]; then
+        echo $OUT
+        FAILED="true"
         EXIT_VAL=$RETURN_VAL
     fi
 
     if [[ -n "$INPUT_CHECKSTD" ]] && [[ "$INPUT_CHECKSTD" == "true" ]]; then
         # Counts occurrences of std:: that aren't in comments and have a leading space (except if it's inside pointer brackets, eg: <std::thing>)
         RETURN_VAL=$(sed -n '/^.*\/\/.*/!s/ std:://p; /^.* std::.*\/\//s/ std:://p; /^.*\<.*std::.*\>/s/std:://p;' "${FILENAME}" | wc -l)
-        if [[ "$RETURN_VAL" -gt "$EXIT_VAL" ]]; then
+        if [[ $RETURN_VAL -gt $EXIT_VAL ]]; then
+            if [[ "$FAILED" != "true" ]]; then
+                echo "${FILENAME} failed style checks."
+            fi
             EXIT_VAL=$RETURN_VAL
         fi
     fi
